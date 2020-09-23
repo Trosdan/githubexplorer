1a9cd096a434d9a375195d559d8d69ead25c7589
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-09-22 21:50:53 -0300

    Updates

---
+++ precache-manifest.c35ac90ef8a00e32cf422f3b96b84beb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b8210c790ef1a482d78e1b51a1d91c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b8decfdb32dbaf53a1",
+    "url": "/static/js/2.00b60e54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.00b60e54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58390f6e2f05b1e2367",
+    "url": "/static/js/main.9c731f1e.chunk.js"
+  },
+  {
+    "revision": "10ac54709e06187be2b5",
+    "url": "/static/js/runtime-main.c61d6a58.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
